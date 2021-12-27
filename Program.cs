b6740c889d556595ac53309e354f1945a5d342fa
mateus queiroz <mateusqueiroz011@gmail.com>
2021-12-27 09:40:19 -0300

    atividade loop whille

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atv_loop_whille
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = new int[10];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = i;
+            }
+
+
+
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine("valor de num: " + i, num[i]);
+            }
+        }
+    }
+}
